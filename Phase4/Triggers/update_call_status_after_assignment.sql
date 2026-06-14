ff5032d1032c976bb485245201d8fc4ec1ffe1af
efratsharabi1 <achygarin.rishon@gmail.com>
2026-06-14 14:58:48 +0300

    Modify call status update trigger message
    Updated the trigger to change the call status message format.

--- Phase4/Triggers/update_call_status_after_assignment.sql
+++ Phase4/Triggers/update_call_status_after_assignment.sql
@@ -3,21 +3,24 @@ RETURNS TRIGGER
 LANGUAGE plpgsql
 AS $$
 BEGIN
+    -- Update the call status after a volunteer is assigned
     UPDATE call
     SET status_id = 2
     WHERE call_id = NEW.call_id;
 
-    RAISE NOTICE 'Call % status updated to 2 after assigning volunteer %',
-        NEW.call_id,
-        NEW.volunteer_id;
+    -- Print a message showing that the trigger was activated
+    RAISE NOTICE
+    'Call % assigned to volunteer %. Status changed from Pending to In Progress',
+    NEW.call_id,
+    NEW.volunteer_id;
 
     RETURN NEW;
 END;
 $$;
 
 
-DROP TRIGGER IF EXISTS trg_update_call_status_after_assignment ON volunteer_call;
 
+-- Create the trigger on volunteer_call after each new assignment
 CREATE TRIGGER trg_update_call_status_after_assignment
 AFTER INSERT
 ON volunteer_call
